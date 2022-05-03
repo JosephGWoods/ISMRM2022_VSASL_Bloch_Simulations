eaa4a0b2e0addd1f7de60ee3139380c14b424b91
Joseph G. Woods <jgmbwoods@gmail.com>
2022-05-03 19:35:31 +0100

    Added Dapeng's corrections to laminarFlowInt.m

--- Plot/laminarFlowInt.m
+++ Plot/laminarFlowInt.m
@@ -8,7 +8,8 @@
 %      Mz_laminar - Mz integrated over velocities
 %      lind       - indicies of mean velocities
 %
-% Written by Joseph G. Woods University of Oxford, April 2022
+% Written by Joseph G. Woods, University of Oxford, April 2022
+% Edited by Dapeng Liu, Johns Hopkins University, May 2022
 
 function [Mz_laminar, lind] = laminarFlowInt(Mz, v)
 
@@ -19,7 +20,7 @@ nv1 = zvind-findNearest(min(v)/2,v); % number of -ve mean velocities we can do t
 nv2 = findNearest(max(v)/2,v)-zvind; % number of +ve mean velocities we can do the laminar integration for [max(v)/2]
 
 % Laminar flow integration (uniform distribution from 0 to Vmax)
-Mz_laminar = zeros(1,nv1+nv2);
+Mz_laminar = zeros(1,nv1+nv2-1);
 
 % Negative velocities
 for ii = 0:nv1-1
@@ -28,12 +29,12 @@ for ii = 0:nv1-1
 end
 
 % Positive velocities
-for ii = 1:nv2
+for ii = 1:nv2-1
     ind = findNearest(2*v(zvind+ii),v);        % find max velocity for current mean velocity (2*meanV)
     Mz_laminar(nv1+ii) = mean(Mz(zvind:1:ind)); % mean Mz across laminar flow profile
 end
 
 % Indices of mean velocities
-lind = zvind-nv1:zvind-1+nv2;
+lind = zvind-nv1+1:zvind+nv2-1;
 
 end
